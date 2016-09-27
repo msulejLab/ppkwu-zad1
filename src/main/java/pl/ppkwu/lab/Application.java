3c8a4b9dcbe954975a21fa4987f8e573cefcb3ce
Maciej Sulej <187809@edu.p.lodz.pl>
2016-09-27 19:54:34 +0200

    wczytywanie danych

--- src/main/java/pl/ppkwu/lab/Application.java
+++ src/main/java/pl/ppkwu/lab/Application.java
@@ -1,8 +1,25 @@
 package pl.ppkwu.lab;
 
+import java.util.Scanner;
+
 public class Application {
+
+    private static Scanner scanner;
 
     public static void main(String[] args) {
         System.out.println("Start..");
+
+        scanner = new Scanner(System.in);
+
+        String fileName = getInput("Nazwa pliku");
+        String correctString = getInput("Poprawny string");
+        String wrongString = getInput("Niepoprawny string");
+
+        System.out.println("Wczytano: " + fileName + ", " + correctString + ", " + wrongString);
+    }
+
+    private static String getInput(String name) {
+        System.out.print("(" + name + "):: ");
+        return scanner.nextLine();
     }
 }
