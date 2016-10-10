f12bc4e293a299c55ad151c5781a7c2d4cb77d29
Maciej Sulej <187809@edu.p.lodz.pl>
2016-10-10 15:44:58 +0200

    refaktoryzacja

--- src/main/java/pl/ppkwu/lab/Application.java
+++ src/main/java/pl/ppkwu/lab/Application.java
@@ -1,67 +1,45 @@
 package pl.ppkwu.lab;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.concurrent.Future;
 
 public class Application {
-  private static final String HELLO_WORLD = "Hello World!";
-  public static final String PLIK_ZOSTAL_UTWORZONY = "Plik zostal utworzony";
-  public static final String FORMAT = "txt";
 
-  private static Scanner scanner;
-  private static FileCallback fileCallback = new FileCallback();
-  private static Boolean fileResult;
+    public static final String HELLO_WORLD = "Hello World";
+    public static final String FORMAT = "txt";
 
+    private static FileCallback fileCallback = new FileCallback();
+    private static Boolean fileResult;
 
-  public static void main(String[] args) {
-    System.out.println("Start..");
+    public static void main(String[] args) {
+        System.out.println("Start..");
 
-    scanner = new Scanner(System.in);
+        String fileName = IOUtils.getInput("Nazwa pliku");
+        String correctString = IOUtils.getInput("Poprawny string");
+        String wrongString = IOUtils.getInput("Niepoprawny string");
+        System.out.println("Wczytano: " + fileName + ", " + correctString + ", " + wrongString);
 
-    String fileName = getInput("Nazwa pliku");
-    String correctString = getInput("Poprawny string");
-    String wrongString = getInput("Niepoprawny string");
-
-    System.out.println("Wczytano: " + fileName + ", " + correctString + ", " + wrongString);
-    try {
-      setData(fileName, correctString, wrongString, fileCallback);
-    } catch (Exception e) {
-      e.getStackTrace();
-    }
-  }
-
-  private static String getInput(String name) {
-    System.out.print("(" + name + "):: ");
-    return scanner.nextLine();
-  }
-
-  private static void setData(String filename, String correctString, String wrongString, FileCallback callback) throws Exception {
-    File file = new File(filename + "." + FORMAT);
-    file.createNewFile();
-    try {
-      System.out.println(correctString);
-      writeToFile(file, HELLO_WORLD);
-      System.out.println(PLIK_ZOSTAL_UTWORZONY);
-      callback.setFile(file);
-      callback.setResult(file.exists());
-      fileResult = callback.call();
-    } catch (IOException IOe) {
-      System.out.println(wrongString);
+        try {
+            setData(fileName, correctString, wrongString, fileCallback);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
+    private static void setData(String filename, String correctString, String wrongString, FileCallback callback) throws Exception {
+        File file = new File(filename + "." + FORMAT);
+        file.createNewFile();
 
-  }
+        try {
+            IOUtils.writeToFile(file, HELLO_WORLD);
+            System.out.println(correctString);
 
-  public static void writeToFile(File file, String message) throws IOException {
-    FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
-    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-    bufferedWriter.write(HELLO_WORLD);
-    bufferedWriter.close();
-  }
+            callback.setFile(file);
+            callback.setResult(file.exists());
 
-  ;
+            fileResult = callback.call();
+        } catch (IOException e) {
+            System.out.println(wrongString);
+        }
+    }
 }
--- src/main/java/pl/ppkwu/lab/FileCallback.java
+++ src/main/java/pl/ppkwu/lab/FileCallback.java
@@ -1,6 +1,5 @@
 package pl.ppkwu.lab;
 
-import javax.security.auth.callback.Callback;
 import java.io.File;
 import java.util.concurrent.Callable;
 
@@ -9,27 +8,24 @@ import java.util.concurrent.Callable;
  */
 public class FileCallback implements Callable<Boolean> {
 
-  private Boolean result = false;
-  private static final String GOD_MESSAGE_TO_FILE = " yeah I'm live and it's my life ";
+    private Boolean result = false;
+    private static final String GOD_MESSAGE_TO_FILE = " yeah I'm live and it's my life ";
 
-  public void setFile(File file) {
-    this.file = file;
-  }
-
-  private File file;
-
-
-  public void setResult(boolean result) {
-    this.result = result;
-  }
-
-
-  public Boolean call() throws Exception {
-    if (result) {
-
+    public void setFile(File file) {
+        this.file = file;
     }
-    return result;
-  }
 
+    private File file;
 
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public Boolean call() throws Exception {
+        if (result) {
+
+        }
+
+        return result;
+    }
 }
